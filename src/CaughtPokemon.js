343ea5d115d71e62764fa09b67b036f76afdad6d
Ryno <rjrfourie@outlook.com>
2021-05-17 00:02:48 +0200

    Complete exercise
    I completed the class exercises. Inserting components

---
+++ src/CaughtPokemon.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CaughtPokemon = (props) => {
+  return (
+  <p>Caught 0 Pokemon on {props.date}</p>
+  );
+};
+
+
+export default CaughtPokemon;
---
+++ src/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Logo = (props) => {
+  console.log(props);
+  return (
+    <header>
+      <h1>Welcome to {props.appName}</h1>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>
+    </header>
+  );
+};
+
+export default Logo;
